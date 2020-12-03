0ed01720247ca8479719408a40dc9c6280749cb0
Kurnosov Dmitriy <dima2202888@yandex.ru>
2020-12-03 23:42:31 +0300

    Fix

--- count_films_by_counties.py
+++ count_films_by_counties.py
@@ -2,17 +2,13 @@ import argparse
 
 from operator import add
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
-from pyspark.sql.types import FloatType
-from operator import itemgetter
-from pyspark.mllib.stat import Statistics
+
+from utils import get_uri_mongodb, update_argument_parser_mongodb
 
 
 def register_launch_arguments():
-    parser = argparse.ArgumentParser(description='Serve the rank correlation calculation')
-    parser.add_argument('-d', '--database', help='database to connect to', required=True)
-    parser.add_argument('-host', '--host', help='server to connect to', default='localhost')
-    parser.add_argument('-port', '--port', help='port to connect to', default=27017)
+    parser = argparse.ArgumentParser(description='Serve the films by each country counting')
+    update_argument_parser_mongodb(parser)
 
     return parser.parse_args()
 
@@ -20,13 +16,14 @@ def register_launch_arguments():
 if __name__ == '__main__':
     args = register_launch_arguments()
 
-    uri = f'mongodb://{args.host}:{args.port}/{args.database}'
+    uri = get_uri_mongodb(args.database, args.username, args.password, args.host, args.port,
+                          args.authenticationDatabase)
     spark = SparkSession.builder.config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.0').getOrCreate()
     df = spark.read.format('com.mongodb.spark.sql.DefaultSource').options(uri=uri, collection='films').load()
 
     df = df.select('data.countries')
 
-    test = df.rdd.flatMap(lambda countries: countries[0]).map(lambda country: (country[0], 1)).reduceByKey(add).collect()
-    test.sort(reverse=True, key=itemgetter(1))
-    print(test)
+    data_films = df.rdd.flatMap(lambda countries: countries[0]).map(lambda country: (country[0], 1)).reduceByKey(add) \
+        .sortBy(lambda x: x[1], ascending=False).collect()
+    print(data_films)
 
