65f70526ded6abdff5d0b2a6a6ea32caee9b4ba5
Kurnosov Dmitriy <dima2202888@yandex.ru>
2020-12-15 22:37:04 +0300

    Fix

--- count_films_by_counties.py
+++ count_films_by_counties.py
@@ -1,5 +1,6 @@
 import argparse
 import plotly.express as px
+import pandas as pd
 
 from operator import add
 
@@ -13,8 +14,9 @@ def register_launch_arguments():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = register_launch_arguments()
+def receiving_and_processing_data():
+    if __name__ == '__main__':
+        args = register_launch_arguments()
 
     df = get_data_frame_from_mongodb(args.database, args.username, args.password, args.host, args.port,
                                      args.authenticationDatabase)
@@ -22,9 +24,19 @@ if __name__ == '__main__':
 
     data_films = df.rdd.flatMap(lambda countries: countries[0]).map(lambda country: (country[0], 1)).reduceByKey(add) \
         .sortBy(lambda x: x[1], ascending=False).collect()
-    countries = list(map(lambda data_film: data_film[0], data_films))[0:100]
-    films = list(map(lambda data_film: data_film[1], data_films))[0:100]
-    #print(*data_films[0:100], sep='\n')
-    fig = px.bar(x=countries, y=films, labels={'x': 'Страна', 'y': 'Количество фильмов'})
+    return data_films
+
+
+def visualization_and_save_data():
+    data_films = receiving_and_processing_data()
+    countries = list(map(lambda data_film: data_film[0], data_films))
+    films = list(map(lambda data_film: data_film[1], data_films))
+
+    frame = pd.DataFrame([countries, films])
+    frame.to_csv('my_csv_export.csv', index=False)
+
+    fig = px.bar(x=countries[0:100], y=films[0:100], labels={'x': 'Страна', 'y': 'Количество фильмов'})
     fig.show()
 
+
+visualization_and_save_data()
